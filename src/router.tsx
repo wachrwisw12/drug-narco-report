744bb63626dd40d4349cf71b3ecf02b692b7342e
wachrwisw12 <wachrwisw12@gmail.com>
2026-01-27 11:39:58 +0700

    dfsf

--- src/router.tsx
+++ src/router.tsx
@@ -4,22 +4,25 @@ import MainLayout from "./layout/MainLayout";
 import ReportPage from "./pages/Reportpage";
 import TrackReportPage from "./pages/Trackpage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "report", element: <ReportPage /> },
+        { path: "track", element: <TrackReportPage /> },
+      ],
+    },
+    //   {
+    //     path: '/login',
+    //     element: <Login />,
+    //   },
+  ],
   {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      { index: true, element: <HomePage /> },
-      { path: "report", element: <ReportPage /> },
-      { path: "track", element: <TrackReportPage /> },
-    ],
+    basename: "/drugnarco",
   },
-  //   {
-  //     path: '/login',
-  //     element: <Login />,
-  //   },
-], {
-    basename: "/drugnaco",
-  });
+);
 
 export default router;
